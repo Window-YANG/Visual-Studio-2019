8247d1e3c50303936a5df62b889837958403eb61
张旭阳 <13588230433@163.com>
2021-02-16 16:15:16 +0800

    更新

---
+++ 算法进阶/旋转数组/旋转数组/旋转数组.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int example[10] = { 1,2,3,4,5,6,7,8,9,12 };
+	int ii = 0,jj = 0, temp[1] = { 0 };
+	int reversize = 3;
+	for (ii = 0; ii < (int)((sizeof(example) / sizeof(example[0]) - 1)/2) + 1 ; ii++)
+	{
+		temp[0] = example[ii];
+		example[ii] = example[(sizeof(example) / sizeof(example[0]) - 1) - ii];
+		example[(sizeof(example) / sizeof(example[0]) - 1) - ii] = temp[0];
+	}
+	
+	for (ii = 0; ii < (int)(reversize / 2); ii++)
+	{
+		temp[0] = example[ii];
+		example[ii] = example[reversize - ii];
+		example[reversize - ii] = temp[0];
+	}
+
+	for (ii = 0; ii < (int)((sizeof(example) / sizeof(example[0]) - reversize) / 2) + 1; ii++)
+	{
+		temp[0] = example[ii + reversize + 1];
+		example[ii + reversize + 1] = example[(sizeof(example) / sizeof(example[0]) - 1) - ii];
+		example[(sizeof(example) / sizeof(example[0]) - 1) - ii] = temp[0];
+	}
+
+
+	for (ii = 0; ii < (sizeof(example) / sizeof(example[0])); ii++)
+	{
+		printf("%d ", example[ii]);
+	}
+
+	return 0;
+
+}
